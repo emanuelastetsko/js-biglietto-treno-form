f7f4b84fce5dda6524ffcddad0d7040afa66dfa4
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2022-12-23 14:15:35 +0100

    Grafica migliorata

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ const nameInput = document.getElementById("name-input");
 const kmInput = document.getElementById("km-input");
 const ageInput = document.getElementById("age-input");
 const submitButton = document.getElementById("submit-button");
+const deleteButton = document.getElementById("delete-button");
 
 // Al click 
 submitButton.addEventListener("click", 
@@ -19,15 +20,29 @@ submitButton.addEventListener("click",
         if (age < 18) {
             let juniorprice = ((km * 0.21) / 100) * 80;
             document.getElementById("ticket-price").innerHTML = juniorprice.toFixed(2) + "€";
+            document.getElementById("offer-type").innerHTML = "Sconto minorenne";
         } else if (age > 65) {
             let seniorprice = ((km * 0.21) / 100) * 60;
             document.getElementById("ticket-price").innerHTML = seniorprice.toFixed(2) + "€";
+            document.getElementById("offer-type").innerHTML = "Sconto silver";
         } else {
             let price = km * 0.21;
             document.getElementById("ticket-price").innerHTML = price.toFixed(2) + "€";
+            document.getElementById("offer-type").innerHTML = "Biglietto standard";
         }
 
         document.getElementById("name-output").innerHTML = nameInput.value;
+
+    }
+                         
+);
+
+deleteButton.addEventListener("click", 
+                         
+    function() {
+        nameInput.value = "";
+        kmInput.value = "";
+        ageInput.value = "";
 
     }
                          
